58d9f1c864010b202cec44d0592fb8f1c9f39836
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-11-20 11:49:03 +0900

    Upgraded case to support number cases

--- src/helpers/case.ts
+++ src/helpers/case.ts
@@ -1,14 +1,41 @@
+function isAlpha(ch: string) {
+	return (ch >= "a" && ch <= "z") || (ch >= "A" && ch <= "Z")
+}
+
+function isNumber(ch: string) {
+	return ch >= "0" && ch <= "9"
+}
+
 // Ex:
 //
 // toKebabCase("HelloWorld") -> "hello-world"
+// toKebabCase("HelloWorld2") -> "hello-world-2"
+// toKebabCase("HelloWorld22") -> "hello-world-22"
 //
 export function toKebabCase(title: string) {
-	return title.replace(/[A-Z]/g, (ch, x) => (!x ? "" : "-") + ch.toLowerCase())
+	let ret = ""
+	for (let x = 0; x < title.length; x++) {
+		let ch = title[x]
+		if (x && ch >= "A" && ch <= "Z") {
+			ret += "-" + ch.toLowerCase()
+			continue
+		} else if (x && isNumber(ch)) {
+			// Must be the start of a sequence:
+			if (x - 1 >= 0 && isAlpha(title[x - 1])) {
+				ret += "-" + ch
+				continue
+			}
+		}
+		ret += ch.toLowerCase()
+	}
+	return ret
 }
 
 // Ex:
 //
 // toTitleCase("hello-world") -> "HelloWorld"
+// toTitleCase("hello-world-2") -> "HelloWorld2"
+// toTitleCase("hello-world-22") -> "HelloWorld22"
 //
 export function toTitleCase(kebab: string) {
 	return kebab
